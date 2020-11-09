8bbf8477b68a1436386b1423b6fa1eac31de5e41
qiaoningg <71680556+qiaoningg@users.noreply.github.com>
2020-11-09 23:45:29 +0800

    Update Chat.vue

--- src/pages/Dashboard/Chat.vue
+++ src/pages/Dashboard/Chat.vue
@@ -1,15 +1,43 @@
 <template>
-    <div>
-        chat
-    </div>
+  <b-container fluid class="h-100">
+    <b-row class="h-100">
+      <b-col
+        cols="12"
+        md="4"
+        lg="3"
+        class="pt-4 h-100"
+        style="background-color: #e5e5e5; display: flex; flex-direction: column"
+      >
+        <b-input placeholder="Search" class="rounded-pill search_input">
+        </b-input>
+        <div style="flex: 1" class=" ">
+          <div
+            class="px-2"
+            :style="{ height: '200px' , overflow: 'auto' }"
+          >
+          </div>
+        </div>
+        </b-col>
+        <b-col class="p-4 h-100"
+          style="display: flex; flex-direction: column">
+        <h2 class="text-muted">
+            <div>
+              <i class="mdi mdi-message display-1"></i>
+            </div>
+            Please select a contact to start chatting
+          </h2>
+        </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-export default {
-    
-}
+
 </script>
 
 <style>
+.search_input {
+  filter: drop-shadow(0px 0px 25px rgba(0, 0, 0, 0.15));
+}
 
 </style>
