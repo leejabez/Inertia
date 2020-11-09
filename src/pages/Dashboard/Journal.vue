5817df067e227327ebf909a5c80b3cfdafa0d8d0
leejabez <68858984+leejabez@users.noreply.github.com>
2020-11-09 17:47:10 +0800

    first commit

---
+++ src/pages/Dashboard/Journal.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        journal
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/routes.js
@@ -0,0 +1,53 @@
+import Index from '../pages/Index.vue'
+import Dashboard from '../pages/Dashboard/View.vue'
+import Login from '../pages/Auth/Login.vue'
+import Signup from '../pages/Auth/Signup.vue'
+import Chat from '../pages/Dashboard/Chat.vue'
+import Discover from '../pages/Discover.vue'
+import Profile from '../pages/Profile.vue'
+import Journal from '../pages/Dashboard/Journal.vue'
+import DashboardIndex from '../pages/Dashboard/Index.vue'
+
+export default [{
+        path: '/',
+        component: Index
+    },
+    {
+        path: '/login',
+        component: Login
+    }, {
+        path: '/signup',
+        component: Signup
+    }, {
+        path: '/dashboard',
+        component: Dashboard,
+        meta: {
+            auth: true
+        },
+        children: [{
+            path: '',
+            component: DashboardIndex
+        }, {
+            path: 'chat',
+            component: Chat
+        }, {
+            path: 'journal',
+            component: Journal
+        }, ]
+    },
+    {
+        path: '/discover',
+        component: Discover,
+        meta: {
+            // auth: true
+        },
+    },
+    {
+        path: '/profile',
+        component: Profile,
+        meta: {
+            // auth: true
+        },
+    },
+
+]
