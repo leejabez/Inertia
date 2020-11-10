884e638a27305eed343a44c005248c850d6717bd
Clem-cpu <53846417+Clem-cpu@users.noreply.github.com>
2020-11-10 09:25:09 +0800

    Update Discover.vue

--- src/pages/Discover.vue
+++ src/pages/Discover.vue
@@ -8,6 +8,22 @@
         <b-col cols="12" class="pt-5 pb-3 pl-5">
           <h2 class="font-weight-bold">Discover.</h2>
         </b-col>
+        <!-- Adding profiles into Discover page from here -->
+        <b-col cols="12">
+          <b-container>
+            <b-row>
+              <template  >
+                <b-col cols="6" md="4" lg="4" xl="3" class="mb-4 text-center">
+                  <div style="cursor:pointer" class="profile_pic mx-auto"></div>
+                  <p style="cursor:pointer" class="font-weight-bold mt-2">Anna</p>
+          
+                </b-col>
+              </template>
+             
+            </b-row>
+          </b-container>
+        </b-col>
+        <!-- Adding profiles into Discover page to here -->
       </b-row>
     </b-container>
     <div class="py-4 text-center">footer</div>
@@ -30,5 +46,14 @@ export default {
   background-position: center;
   background-size: cover;
   background-attachment: fixed;
+}
+.profile_pic {
+  width: 170px;
+  height: 170px;
+  background: url("../assets/discover.png") no-repeat;
+  background-position: center;
+  background-size: cover;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 45px;
 }
 </style>
