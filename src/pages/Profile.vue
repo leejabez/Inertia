774fad0bcf65800ed832aee8d5bca978d85cc1cf
Clem-cpu <53846417+Clem-cpu@users.noreply.github.com>
2020-11-10 08:53:18 +0800

    Update Profile.vue

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,14 +1,35 @@
-<template>            
-    <b-col cols="12">
-    <div v-for="(v, k) in characteristics" :key="k">
-        <h4 class="text-fer" style="text-decoration: underline">
-        {{ v.title }}
-        </h4>
-        <p>
-        {{ v.text }}
-        </p>
-    </div>
-    </b-col>
+<template>
+  <b-overlay>
+    <b-container fluid>
+      <!-- Back button from here -->
+      <b-col cols="12">
+          <div class="my-4">
+            <b-btn
+              class="back_btn float-left"
+              style=""
+              @click="$router.replace('/discover')"
+            >
+              <i class="mdi mdi-keyboard-backspace"></i>
+            </b-btn>
+          </div>
+        </b-col>
+      <!-- Back button to here -->
+          <b-container>
+            <b-row>
+              <b-col cols="12">
+                <div v-for="(v, k) in characteristics" :key="k">
+                  <h4 class="text-fer" style="text-decoration: underline">
+                    {{ v.title }}
+                  </h4>
+                  <p>
+                    {{ v.text }}
+                  </p>
+                </div>
+              </b-col>
+            </b-row>
+          </b-container>
+    </b-container>
+  </b-overlay>
 </template>
 
 <script>
