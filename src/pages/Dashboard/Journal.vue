32c0cea946f6753116837637078a736a1fcfa3e1
nellyloh <59547687+nellyloh@users.noreply.github.com>
2020-11-11 13:01:21 +0800

    Added calendar function for Journal Page

--- src/pages/Dashboard/Journal.vue
+++ src/pages/Dashboard/Journal.vue
@@ -1,7 +1,18 @@
 <template>
-    <div>
-        <h2 class="font-weight-bold">Journal.</h2>
-    </div>
+      <div class="main_journal">
+    <div
+      :class="[
+        'd-inline-block c_c_default',
+        { calendar_custom: !loadVisibleClass },
+      ]"
+      @mouseenter="loadVisibleClass = true"
+      @mouseleave="loadVisibleClass = false"
+    >
+      <b-calendar
+        v-model="selectedDate"
+        label-help="Hover to change the day"
+        locale="en-US"
+      ></b-calendar>
 </template>
 
 <script>
