459be46d9db86a2c4fddd54a8fbc9fe3907bf843
nellyloh <59547687+nellyloh@users.noreply.github.com>
2020-11-11 13:07:51 +0800

    Added in notes feature for Journal Page

--- src/pages/Dashboard/Journal.vue
+++ src/pages/Dashboard/Journal.vue
@@ -1,7 +1,25 @@
 <template>
-    <div>
-        <h2 class="font-weight-bold">Journal.</h2>
-    </div>
+        <div
+      class=""
+      style="padding: 150px; display: flex; justify-content: center"
+    >
+      <div class="notes">
+        <div
+          class="border-bottom py-3 text-white font-weight-bold d-flex justify-content-between"
+        >
+          <div class="pl-4">
+            <i class="mdi mdi-chevron-left h4" style="cursor: pointer"></i>
+          </div>
+          <div>{{ yesterDayValue }}</div>
+          <div class="pr-4"></div>
+        </div>
+        <div
+          style="flex: 1; height: 320px; overflow: auto"
+          class="p-3 text-white font-weight-bold"
+        >
+          {{ yesterDayNote.text || "" }}
+        </div>
+      </div>
 </template>
 
 <script>
