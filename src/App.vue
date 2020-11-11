292171718bd492d6af1275fb40ad11c03d361030
nellyloh <59547687+nellyloh@users.noreply.github.com>
2020-11-12 00:43:04 +0800

    Updated header feature

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <template>
   <div id="app">
     <h1> Home Page </h1>
+     <header-view />
+    <div>
+                <div
+                  style="display: flex; align-items: center; cursor: pointer"
+                  class="pb-1 border-bottom"
+                  @click="loadedContact = null"
+                >
+                  <i
+                    class="mdi mdi-chevron-left"
+                    style="line-height: 35px; font-size: 35px"
+                  ></i>
+                  <b-avatar class="bg-secondary"></b-avatar>
+                  <div class=" " style="flex: 1; display: flex">
+                    <div
+                      class="text-left pl-2"
+                      style="flex: 1; display: flex; flex-direction: column"
+                    >
+                      <span class="font-weight-bold">{{
+                        loadedContact.name
+                      }}</span>
+                      <span class="text-muted"
+                        >status . Last seen 2 hour ago</span
+                      >
+                    </div>
+                  </div>
+                </div>
+              </div>
   </div>
 </template>
 
@@ -14,13 +41,22 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped >
+@import url("https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.8.55/css/materialdesignicons.min.css");
+@import url("https://fonts.googleapis.com/css2?family=Heebo:wght@700&display=swap");
+@import url("./assets/scss/custom.scss");
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.bg-fer {
+  background-color: #0094b6 !important;
+}
+.text-fer {
+  color: #0094b6 !important;
 }
 </style>
