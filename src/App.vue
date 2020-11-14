447e20bf8ac21765d0916f06b4c603b15f779fb8
qiaoningg <qiaoning8@gmail.com>
2020-11-15 00:36:06 +0800

    Update App.vue
    remove status

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,6 @@
                       style="flex: 1; display: flex; flex-direction: column"
                     >
                       <span class="font-weight-bold">{{ v.userData.name }}</span>
-                      <span class="text-muted">status</span>
                     </div>
                     <div class="d-flex align-items-center text-muted">
                       {{ formatedDate(v.last_message_at) }}
@@ -72,9 +71,7 @@
                       <span class="font-weight-bold">{{
                         loadedContact.userData.name
                       }}</span>
-                      <span class="text-muted"
-                        >status . Last seen 2 hour ago</span
-                      >
+                  
                     </div>
                   </div>
                 </div>
