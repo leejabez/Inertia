83562d047acd788d6f97ea798d62437f35691394
leejabez <68858984+leejabez@users.noreply.github.com>
2020-11-14 17:07:29 +0800

    Changing sign up page

--- src/pages/Auth/Signup.vue
+++ src/pages/Auth/Signup.vue
@@ -81,15 +81,79 @@
 </template>
 
 <script>
+import footerView from "@/components/footer";
 export default {
-    
-}
+  data() {
+    return {
+      name: null,
+      email: null,
+      password: null,
+      cPassword: null,
+      agreed: false,
+      loading: false,
+    };
+  },
+  computed: {
+    isValid() {
+      return (
+        this.name != null &&
+        this.email != null &&
+        this.password != null &&
+        this.cPassword == this.password &&
+        this.agreed
+      );
+    },
+  },
+  components: {
+    footerView,
+  },
+  methods: {
+    reset() {
+      this.loading = false;
+      this.email = null;
+      this.password = null;
+      this.cPassword = null;
+      this.name = null;
+    },
+
+    signup(e) {
+      e.preventDefault();
+      this.loading = true;
+      var self = this;
+      this.$fb
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then((user) => {
+          console.log(user);
+          this.$fb
+            .firestore()
+            .collection("users")
+            .doc(user.user.uid)
+            .set({
+              uid: user.user.uid,
+              name: self.name,
+              email: self.email,
+
+              sent: 0,
+              task_completed: 0,
+              time_online: 0,
+              blocked_users: 0,
+            })
+            .then(() => {
+              self.$router.push("/dashboard");
+              self.reset();
+            })
+            .catch((err) => {
+              alert("an error occured ! " + err);
+            });
+        })
+        .catch((err) => {
+          alert(err);
+          this.reset();
+        });
+    },
+  },
+};
 </script>
 
-<style>
-.signup_img {
-  background: url("../../assets/signup_img.png") no-repeat;
-  background-position: center;
-  background-size: cover;
-}
-</style>
+ 
